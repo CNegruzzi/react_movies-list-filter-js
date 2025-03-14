942ee20081d0078b5b52040cb3f80a59d9f0f701
Cirillo Negruzzi <assus.12345nagi@gmail.com>
2025-03-14 18:00:38 +0100

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,30 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, filter }) => {
+  function filterMovies(queries, movie) {
+    const formattedQuery = queries.trim().toLowerCase();
+    const formattedMovieTitle = movie.title.trim().toLowerCase();
+    const formattedDescription = movie.description.trim().toLowerCase();
+
+    if (
+      formattedMovieTitle.includes(formattedQuery) ||
+      formattedDescription.includes(formattedQuery)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  return (
+    <div className="movies">
+      {movies.map(
+        movie =>
+          filterMovies(filter, movie) && (
+            <MovieCard key={movie.imdbId} movie={movie} />
+          ),
+      )}
+    </div>
+  );
+};
