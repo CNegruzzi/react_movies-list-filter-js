0a4e55c4c7818a80db8e6b9918cc541abd30363c
Cirillo Negruzzi <assus.12345nagi@gmail.com>
2025-03-14 18:11:10 +0100

    added changes

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,22 +1,22 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies, filter }) => {
-  function filterMovies(queries, movie) {
-    const formattedQuery = queries.trim().toLowerCase();
-    const formattedMovieTitle = movie.title.trim().toLowerCase();
-    const formattedDescription = movie.description.trim().toLowerCase();
+function filterMovies(searchParams, movie) {
+  const formattedQuery = searchParams.trim().toLowerCase();
+  const formattedMovieTitle = movie.title.trim().toLowerCase();
+  const formattedDescription = movie.description.trim().toLowerCase();
 
-    if (
-      formattedMovieTitle.includes(formattedQuery) ||
-      formattedDescription.includes(formattedQuery)
-    ) {
-      return true;
-    }
-
-    return false;
+  if (
+    formattedMovieTitle.includes(formattedQuery) ||
+    formattedDescription.includes(formattedQuery)
+  ) {
+    return true;
   }
 
+  return false;
+}
+
+export const MoviesList = ({ movies, filter }) => {
   return (
     <div className="movies">
       {movies.map(
